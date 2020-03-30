c9dcd2c8782c7b37e6c021e16b471a12d0b672c2
cthoughtz <cthoughtz@gmail.com>
2020-03-30 02:57:43 -0500

    added call feature

--- app/src/main/java/com/example/hotel/view/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/hotel/view/Fragments/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.example.hotel.view.Fragments
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.hotel.R
 import com.example.hotel.view.Activities.HomeScreen
+import kotlinx.android.synthetic.main.fragment_search.*
 import kotlinx.android.synthetic.main.homescreen_main.*
 
 class SearchFragment : Fragment() {
@@ -23,7 +26,19 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        (activity as HomeScreen).toolbar.title = "Search"
+
+        callHotel.setOnClickListener{
+
+            callHotel()
+        }
+    }
+
+
+    fun callHotel(){
+
+        val callLocalNumber = Intent(Intent.ACTION_DIAL)
+        callLocalNumber.setData(Uri.parse("tel:${getString(R.string.toll_free_number)}"))
+        startActivity(callLocalNumber)
     }
 
 }
