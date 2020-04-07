eb707d0de152577d44ebdf390dc9053ca2f6032f
cthoughtz <cthoughtz@gmail.com>
2020-04-07 11:57:50 -0500

    update toolbar

--- app/src/main/java/com/example/hotel/view/Activities/CheckIn.kt
+++ app/src/main/java/com/example/hotel/view/Activities/CheckIn.kt
@@ -3,11 +3,18 @@ package com.example.hotel.view.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hotel.R
+import kotlinx.android.synthetic.main.activity_check_in.*
 
 class CheckIn : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_in)
+
+        //setup toolbar
+       setSupportActionBar(check_in_toolbar)
+        val checkInToolbar = supportActionBar
+        checkInToolbar?.title = "Reservation"
+        checkInToolbar?.setDisplayHomeAsUpEnabled(true)
     }
 }
