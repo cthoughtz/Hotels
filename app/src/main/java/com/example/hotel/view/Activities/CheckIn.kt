78f502ac22f2ce5ffa151d4726860386e540fa85
cthoughtz <cthoughtz@gmail.com>
2020-04-07 14:55:24 -0500

    added implementation to back arrow

--- app/src/main/java/com/example/hotel/view/Activities/CheckIn.kt
+++ app/src/main/java/com/example/hotel/view/Activities/CheckIn.kt
@@ -1,7 +1,10 @@
 package com.example.hotel.view.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.hotel.R
 import kotlinx.android.synthetic.main.activity_check_in.*
 
@@ -16,5 +19,16 @@ class CheckIn : AppCompatActivity() {
         val checkInToolbar = supportActionBar
         checkInToolbar?.title = "Reservation"
         checkInToolbar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == android.R.id.home) {
+
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
