fd942c5f98e2ada5c851a78f725da030be17b55a
cthoughtz <cthoughtz@gmail.com>
2020-04-10 08:30:17 -0500

    added Hotel Deals Activity

---
+++ app/src/main/java/com/example/hotel/view/Activities/HotelDeals.kt
@@ -0,0 +1,12 @@
+package com.example.hotel.view.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HotelDeals : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotel_deals)
+    }
+}
--- app/src/main/java/com/example/hotel/view/Activities/Reservation.kt
+++ app/src/main/java/com/example/hotel/view/Activities/Reservation.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import com.example.hotel.AppUtilities
 import com.example.hotel.R
-import com.jakewharton.rxbinding2.view.activated
 import kotlinx.android.synthetic.main.activity_check_in.*
 
 class Reservation : AppCompatActivity() {
@@ -33,7 +33,7 @@ class Reservation : AppCompatActivity() {
                 adultPersonCounter++
             }
 
-            numberOfAddultsPerRoom(adultPersonCounter)
+            numberOfAdultsPerRoom(adultPersonCounter)
         }
 
         btn_remove_adult.setOnClickListener {
@@ -42,11 +42,87 @@ class Reservation : AppCompatActivity() {
                 adultPersonCounter--
             }
 
-            numberOfAddultsPerRoom(adultPersonCounter)
+            numberOfAdultsPerRoom(adultPersonCounter)
+        }
+
+
+        btn_add_children.setOnClickListener {
+
+            if (childPersonCounter < 3){
+                childPersonCounter++
+            }
+
+            numberOfChildrenPerRoom(childPersonCounter)
+        }
+
+        btn_remove_children.setOnClickListener {
+
+            if(childPersonCounter > 0){
+                childPersonCounter--
+            }
+            numberOfChildrenPerRoom(childPersonCounter)
+        }
+
+        textView20.setOnClickListener {
+
+            Toast.makeText(this,"No functionality for Demo",Toast.LENGTH_LONG).show()
+        }
+
+        btn_show_deals.setOnClickListener {
+
+            val intent = Intent(this,HotelDeals::class.java)
+            startActivity(intent)
         }
     }
 
-    private fun numberOfAddultsPerRoom(adultPersonCounter: Int) {
+    private fun numberOfChildrenPerRoom(childPersonCounter: Int){
+
+        child_text_count.text = childPersonCounter.toString()
+
+        btn_remove_children.isActivated = childPersonCounter>0
+
+        when(childPersonCounter){
+
+            0->{
+                view_three.visibility = View.GONE
+                text_child_1.visibility = View.GONE
+                spinner_child_1.visibility = View.GONE
+                child_text_count_1.visibility = View.GONE
+            }
+            1 -> {
+                view_three.visibility = View.VISIBLE
+                text_child_1.visibility = View.VISIBLE
+                spinner_child_1.visibility = View.VISIBLE
+                child_text_count_1.visibility = View.VISIBLE
+
+                view_four.visibility = View.GONE
+                text_child_2.visibility = View.GONE
+                spinner_child_2.visibility = View.GONE
+                child_text_count_2.visibility = View.GONE
+            }
+            2 ->{
+
+                view_four.visibility = View.VISIBLE
+                text_child_2.visibility = View.VISIBLE
+                spinner_child_2.visibility = View.VISIBLE
+                child_text_count_2.visibility = View.VISIBLE
+
+                view_five.visibility = View.GONE
+                text_child_3.visibility = View.GONE
+                spinner_child_3.visibility = View.GONE
+                child_text_count_3.visibility = View.GONE
+            }
+            3 ->{
+                view_five.visibility = View.VISIBLE
+                text_child_3.visibility = View.VISIBLE
+                spinner_child_3.visibility = View.VISIBLE
+                child_text_count_3.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    private fun numberOfAdultsPerRoom(adultPersonCounter: Int) {
 
         adult_text_count.text = adultPersonCounter.toString()
 
