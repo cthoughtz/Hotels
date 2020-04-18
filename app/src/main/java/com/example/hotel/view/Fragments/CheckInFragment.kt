57023bad69f3d10b46bbc8a9f56ec3b57cde7b68
cthoughtz <cthoughtz@gmail.com>
2020-04-18 11:13:00 -0500

    saved selected date to shared preference

--- app/src/main/java/com/example/hotel/view/Fragments/CheckInFragment.kt
+++ app/src/main/java/com/example/hotel/view/Fragments/CheckInFragment.kt
@@ -1,12 +1,16 @@
 package com.example.hotel.view.Fragments
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.applandeo.materialcalendarview.CalendarView
 import com.example.hotel.R
 import kotlinx.android.synthetic.main.fragment_check_in.*
 import java.util.*
@@ -17,14 +21,48 @@ import java.util.*
  */
 class CheckInFragment : Fragment() {
 
+    val TAG = javaClass.simpleName
+    val selectedYear = "SELECTED_YEAR"
+    val selectedMonth = "SELECTED_MONTH"
+    val selectedDay = "SELECTED_DAY"
+
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_in, container, false)
 
+        return inflater.inflate(R.layout.fragment_check_in, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        val sharedPrefs = activity?.getSharedPreferences("Dates", Context.MODE_PRIVATE)
+        val selectedDate = calendarView.selectedDates
+
+
+
+
+        calendarView.setOnDayClickListener {
+
+            // get the updated day of the month after click takes place
+            val currentDayOfMonth = it.calendar
+            var calendarDay= currentDayOfMonth.get(Calendar.DAY_OF_MONTH)
+            var year = currentDayOfMonth.get(Calendar.YEAR)
+            var month = currentDayOfMonth.get(Calendar.MONTH)
+
+            sharedPrefs?.edit()?.putString(selectedYear,year.toString())?.commit()
+            sharedPrefs?.edit()?.putString(selectedMonth,month.toString())?.commit()
+            sharedPrefs?.edit()?.putString(selectedDay,calendarDay.toString())?.commit()
+
+
+            //Todo add to shared preference and set date selected in checkout and UI.
+        }
+
+    }
 }
+
