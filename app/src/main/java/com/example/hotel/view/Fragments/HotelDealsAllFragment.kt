aea9138073abd117a65e62a95c156e2268c78bd0
cthoughtz <cthoughtz@gmail.com>
2020-04-21 17:16:49 -0500

    Implemented Room  Booked Percent CardView

--- app/src/main/java/com/example/hotel/view/Fragments/HotelDealsAllFragment.kt
+++ app/src/main/java/com/example/hotel/view/Fragments/HotelDealsAllFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dinuscxj.progressbar.CircleProgressBar
 import com.example.hotel.R
+import com.example.hotel.view.Activities.HotelDeals
+import kotlinx.android.synthetic.main.fragment_hotel_deals_all.*
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +22,36 @@ class HotelDealsAllFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hotel_deals_all, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val sq = (activity as HotelDeals)
+        setUpBookedPercentagCardView(sq.searchQuery)
+    }
+
+    private fun setUpBookedPercentagCardView(searchQuery: String) {
+
+        var DEFAUL_PATTERN = "%d%%"
+        var random = (1..100).random()
+
+        val progress = object: CircleProgressBar.ProgressFormatter{
+            override fun format(progress: Int, max: Int): CharSequence {
+
+                return String.format(DEFAUL_PATTERN, random)
+            }
+        }
+
+        circleProgressBar2.progress = random
+        circleProgressBar2.setProgressFormatter(progress)
+
+        bookedPercentText.text = "We are $random% booked! $searchQuery is popular for your travel dates."
+
+        closeBookedPercentTextView.setOnClickListener{
+
+            bookedPercentContraint.visibility = View.GONE
+        }
     }
 
 }
